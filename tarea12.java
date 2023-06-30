05af3e33a3774abf1e7bd007b4ae582b65671a56
JuanPayero <137441934+JuanPayero@users.noreply.github.com>
2023-06-30 17:57:56 -0300

    Update and rename tarea12.txt to tarea12.java

--- tarea12.java
+++ tarea12.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class DocenaNumero {
+public class tarea12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
