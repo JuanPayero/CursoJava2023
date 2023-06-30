0c872f31268477d4ff7da33d70893bcfda543fe3
JuanPayero <137441934+JuanPayero@users.noreply.github.com>
2023-06-30 18:02:32 -0300

    Update and rename tarea7.txt to tarea7.java

--- tarea7.java
+++ tarea7.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class MayorDeTres {
+public class tarea7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
