2f19b51d49f62293a4bb3583e37c8559973e3a35
JuanPayero <137441934+JuanPayero@users.noreply.github.com>
2023-06-30 18:00:16 -0300

    Update and rename tarea18.txt to tarea18.java

--- tarea18.java
+++ tarea18.java
@@ -1,5 +1,5 @@
 
-public class TabladeMultiplicarAnidadas {
+public class tarea18 {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             System.out.println("Tabla del " + i + ":");
