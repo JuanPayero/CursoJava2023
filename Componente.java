572f85213dc3c6302dc2032d8bba0145549beb81
JuanPayero <137441934+JuanPayero@users.noreply.github.com>
2023-08-12 15:34:07 -0300

    Update Componente.java

--- Componente.java
+++ Componente.java
@@ -1,37 +1,39 @@
+
 public class Componente {
-	private String nombre;
-	private float valor;
-	private String unidad;
-	private int frecuencia;
-	//construcctores
-	
-	//accessors
-	public void setNombre(String pNom){
-		nombre = pNom;
+
+		private String nombre;
+		private float valor;
+		private String unidad;
+		private int frecuencia;
+		//constructores
+		
+		//accessors
+		public void setNombre(String pNom){
+			nombre = pNom;
+		}
+		public String getNombre(){
+			return nombre;
+		}
+		public void setUnidad(String pUni){
+			unidad = pUni;
+		}
+		public String getUnidad(){
+			return unidad;
+		}
+		public void setValor(float pVal){
+			valor = pVal;
+		}
+		public float getValor(){
+			return valor;
+		}
+		public void setFrecuencia(int pFrec){
+			frecuencia = pFrec;
+		}
+		public int getFrecuencia(){
+			return frecuencia;
+		}
+		
+		
+		//negocio
+		
 	}
-	public String getNombre(){
-		return nombre;
-	}
-	public void setUnidad(String pNom){
-		unidad = pNom;
-	}
-	public String getUnidad(){
-		return unidad;
-	}
-	public void setValor(float pNom){
-		nombre = pNom;
-	}
-	public float getValor(){
-		return valor;
-	}
-	public void setFrecuencia(int pNom){
-		frecuencia = pNom;
-	}
-	public int getFrecuencia(){
-		return frecuencia;
-	}
-	
-	
-	//negocio
-	
-}
